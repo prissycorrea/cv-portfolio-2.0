29c652d1afeee788aa302f49b2c932bb2564ded9
PrissyCorrea <pris.pcd@gmail.com>
2025-05-05 15:46:45 -0300

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import AnimatedCounter from "./components/AnimatedCounter"
-import Hero from "./sections/Hero"
-import ShowcaseSection from "./sections/ShowcaseSection"
+import AnimatedCounter from "./components/AnimatedCounter";
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <ShowcaseSection />
     </>
