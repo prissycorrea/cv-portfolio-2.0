89032ceb34ab1e1ad82798c055c7d09cdede138c
PrissyCorrea <pris.pcd@gmail.com>
2025-05-05 16:10:03 -0300

    logo showcase section created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AnimatedCounter from "./components/AnimatedCounter";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import Navbar from "./components/Navbar";
+import LogoSection from "./sections/LogoSection";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
     </>
   )
 }
