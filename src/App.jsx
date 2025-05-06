9b9473c9e8ecc0e2868e8a36565cdb421439f715
PrissyCorrea <pris.pcd@gmail.com>
2025-05-06 14:19:28 -0300

    footer section created

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
       <TechStack />
       <Testimonials />
       <Contact />
+      <Footer />
     </>
   )
 }
