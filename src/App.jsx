55df94fe055332fbd2e58db0a9b847400f04ea24
PrissyCorrea <pris.pcd@gmail.com>
2025-05-06 13:43:13 -0300

    testimonials section created

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import LogoSection from "./sections/LogoSection";
 import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <FeatureCards />
       <ExperienceSection />
       <TechStack />
+      <Testimonials />
     </>
   )
 }
