9148e0e1d033ad74bb33bc40a90abb46edc6454f
PrissyCorrea <pris.pcd@gmail.com>
2025-05-06 14:14:24 -0300

    contact section created

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <ExperienceSection />
       <TechStack />
       <Testimonials />
+      <Contact />
     </>
   )
 }
